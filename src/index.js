8c83a3c15b00b39f1d105c8cb1db6056203ef665
vishal <vishal.parmar@kevit.io>
2022-07-14 12:50:48 +0530

    Use Async & await

--- src/index.js
+++ src/index.js
@@ -9,80 +9,89 @@ app.use(express.json())
 
 //* Add New User
 
-app.post("/users",(req,res)=>{
+app.post("/users", async (req, res) => {
     const user = new User(req.body);
-
-    user.save().then(()=>{
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((error)=>{
-        res.status(400).send(error.message)
-    })
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 //* Get All Users.
 
-app.get("/users",(req,res) => {
-    User.find({}).then((users)=>{
-        res.status(200).send(users)
-    }).catch((e)=>{
-        res.status(500).send()
-    })
+app.get("/users", async (req, res) => {
+    try {
+        await User.find({}).then((users) => { res.status(200).send(users) })
+    } catch {
+        (e) => {
+            res.status(500).send(e)
+        }
+    }
 })
 
 //* Get One User.
 
-app.get("/user/:id",(req,res)=>{
-    const _id = req.params.id;
-
-    User.findById(_id).then((user)=>{
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }).catch((error)=>{
-        res.status(500).send()
-    })
+app.get("/user/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        await User.findById(_id).then((user) => {
+            if (!user) {
+                return res.status(404).send()
+            }
+            res.send(user)
+        })
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 //* Add New Task
 
-app.post("/tasks",(req,res)=>{
-    const task = new Task(req.body);
-
-    task.save().then(()=>{
+app.post("/tasks", async (req, res) => {
+    try {
+        const task = new Task(req.body);
+        await task.save()
         res.status(201).send(task)
-    }).catch((error)=>{
-        res.status(400).send(error.message)
-    })
+    }
+    catch (e) {
+        res.status(400).send(e)
+
+    }
 })
 
 //* Get All Tasks.
 
-app.get("/tasks",(req,res)=>{
-    Task.find({}).then((tasks)=>{
-        res.status(200).send(tasks)
-    }).catch((error)=>{
-        res.status(500).send()
-    })
+app.get("/tasks", async (req, res) => {
+    try {
+        await Task.find({}).then((tasks) => {
+            res.status(200).send(tasks)
+        })
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 //* Get One Task
 
-app.get("/task/:id",(req,res)=>{
-    const _id = req.params.id;
-
-    Task.findById(_id).then((task)=>{
-        if(!task){
-            return res.status(404).send()
-        }
-        res.send(task)
-    }).catch((error)=>{
-        res.status(500).send()
-    })
+app.get("/task/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        await Task.findById(_id).then((task) => {
+            if (!task) {
+                return res.status(404).send()
+            }
+            res.send(task)
+        })
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 //! Listen App In Port
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`App Is Listen On ${PORT} Go To:- http://localhost:${PORT}/users`);
 })
