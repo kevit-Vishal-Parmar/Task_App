9f7a11282bd8080131e50d8bb6e7b38c0b667be9
vishal <vishal.parmar@kevit.io>
2022-07-14 18:38:32 +0530

    Add User And Task Router In App

---
+++ src/routers/task-router.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const { model, models } = require("mongoose");
+const Task = require("../models/task");
+const router = new express.Router();
+
+//* Add New Task
+
+router.post("/tasks", async (req, res) => {
+    try {
+        const task = new Task(req.body);
+        await task.save()
+        res.status(201).send(task)
+    }
+    catch (e) {
+        res.status(400).send(e)
+
+    }
+})
+
+//* Get All Tasks.
+
+router.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).send(tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+//* Get One Task
+
+router.get("/task/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+//* Update Task
+
+router.patch("/task/:id", async (req, res) => {
+    const allowedUpdate = ["description", "complete"];
+    const updates = Object.keys(allowedUpdate);
+    const isValidation = updates.every((update) => allowedUpdate.includes(update))
+
+    if (!isValidation) {
+        return res.status(400).send({ error: "Invalid Update!" })
+    }
+    try {
+        const _id = req.params.id;
+        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//*Delete a Task
+
+router.delete("/task/:id",async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const task = await Task.findByIdAndDelete(_id);
+        if (!task) {
+            res.status(404).status({error : "Task Is Not Exits"})
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+module.exports = router;
---
+++ src/routers/user-router.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const User = require("../models/user");
+const router = new express.Router();
+// require("../db/mongoose")
+
+//* Add New User
+
+router.post("/users", async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//* Get All Users.
+
+router.get("/users", async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.status(200).send(users)
+    } catch {
+        (e) => {
+            res.status(500).send(e)
+        }
+    }
+})
+
+//* Get One User.
+
+router.get("/user/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+//* Update User.
+
+router.patch("/user/:id", async (req, res) => {
+    const allowedUpdate = ["name", "age", "email", "password"];
+    const updates = Object.keys(allowedUpdate);
+    const isValidation = updates.every((update) => allowedUpdate.includes(update))
+
+    if (!isValidation) {
+        return res.status(400).send({ error: "Invalid Update!" })
+    }
+    try {
+        const _id = req.params.id;
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        if (!user) {
+            res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//*Delete a User
+
+router.delete("/user/:id",async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const user = await User.findByIdAndDelete(_id);
+        if (!user) {
+            res.status(404).status({error : "User Is Not Exits"})
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+module.exports = router
